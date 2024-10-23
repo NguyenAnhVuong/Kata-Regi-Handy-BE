37a2b6717cf9fd68ca3f04a66b1f4ac402543318
NguyenAnhVuong <vuong30801.workspace@gmail.com>
2024-10-23 20:47:49 +0700

    [Feat] Create table group

--- src/modules/table/table.service.ts
+++ src/modules/table/table.service.ts
@@ -1,14 +1,13 @@
-import { InjectRepository } from '@nestjs/typeorm';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Table } from '@core/database/entity/table.entity';
-import { DataSource, EntityManager, Repository } from 'typeorm';
 import { OrderService } from '@modules/order/order.service';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, EntityManager, In, Repository } from 'typeorm';
 // import { OrderItemService } from '@modules/order-item/order-item.service';
+import { ErrorMessage, ETableStatus } from '@core/enum';
 import { IUserData } from '@core/interface/default.interface';
-import { UpdateTableInput } from './dto/update-table.input';
-import { Order } from '@core/database/entity/order.entity';
-import { EOrderStatus, ErrorMessage, ETableStatus } from '@core/enum';
 import { CreatePaymentInput } from './dto/create-payment.input';
+import { UpdateTableInput } from './dto/update-table.input';
 
 @Injectable()
 export class TableService {
@@ -16,48 +15,48 @@ export class TableService {
     @InjectRepository(Table)
     private readonly tableRepository: Repository<Table>,
     private readonly dataSource: DataSource,
-    private readonly orderService: OrderService,
-    // private readonly orderItemService: OrderItemService,
-  ) { }
-
+    private readonly orderService: OrderService, // private readonly orderItemService: OrderItemService,
+  ) {}
 
   async getTables(userData: IUserData) {
-
     return await this.tableRepository
-    .createQueryBuilder("table")
-    .leftJoin("table.orders", "order")
-    .leftJoin("order.orderItems", "orderItem")
-    .select("table.id", "id")
-    .addSelect("table.name", "name")
-    .addSelect("table.restaurantId", "restaurantId")
-    .addSelect("table.status", "status")
-    .addSelect("table.amountOfPeople", "amountOfPeople")
-    .addSelect("table.openAt", "openAt")
-    .addSelect("COALESCE(SUM(CASE WHEN order.status != 'COMPLETED' THEN orderItem.price * orderItem.quantity ELSE 0 END), 0)", "total")
-    .groupBy("table.id")
-    .orderBy("table.id")
-    .getRawMany();
-
+      .createQueryBuilder('table')
+      .leftJoin('table.orders', 'order')
+      .leftJoin('order.orderItems', 'orderItem')
+      .select('table.id', 'id')
+      .addSelect('table.name', 'name')
+      .addSelect('table.restaurantId', 'restaurantId')
+      .addSelect('table.status', 'status')
+      .addSelect('table.amountOfPeople', 'amountOfPeople')
+      .addSelect('table.openAt', 'openAt')
+      .addSelect(
+        "COALESCE(SUM(CASE WHEN order.status != 'COMPLETED' THEN orderItem.price * orderItem.quantity ELSE 0 END), 0)",
+        'total',
+      )
+      .groupBy('table.id')
+      .orderBy('table.id')
+      .getRawMany();
   }
 
   async getUnpaidTables(userData: IUserData) {
-
     return await this.tableRepository
-    .createQueryBuilder("table")
-    .leftJoin("table.orders", "order")
-    .leftJoin("order.orderItems", "orderItem")
-    .select("table.id", "id")
-    .addSelect("table.name", "name")
-    .addSelect("table.restaurantId", "restaurantId")
-    .addSelect("table.status", "status")
-    .addSelect("table.amountOfPeople", "amountOfPeople")
-    .addSelect("table.openAt", "openAt")
-    .addSelect("COALESCE(SUM(CASE WHEN order.status != 'COMPLETED' THEN orderItem.price * orderItem.quantity ELSE 0 END), 0)", "total")
-    .where("table.status = :inuseStatus", {inuseStatus: ETableStatus.INUSE})
-    .groupBy("table.id")
-    .orderBy("table.id")
-    .getRawMany();
-
+      .createQueryBuilder('table')
+      .leftJoin('table.orders', 'order')
+      .leftJoin('order.orderItems', 'orderItem')
+      .select('table.id', 'id')
+      .addSelect('table.name', 'name')
+      .addSelect('table.restaurantId', 'restaurantId')
+      .addSelect('table.status', 'status')
+      .addSelect('table.amountOfPeople', 'amountOfPeople')
+      .addSelect('table.openAt', 'openAt')
+      .addSelect(
+        "COALESCE(SUM(CASE WHEN order.status != 'COMPLETED' THEN orderItem.price * orderItem.quantity ELSE 0 END), 0)",
+        'total',
+      )
+      .where('table.status = :inuseStatus', { inuseStatus: ETableStatus.INUSE })
+      .groupBy('table.id')
+      .orderBy('table.id')
+      .getRawMany();
   }
 
   async updateTable(userData: IUserData, updateTableInput: UpdateTableInput) {
@@ -70,41 +69,69 @@ export class TableService {
     if (!table) {
       throw new HttpException(
         ErrorMessage.TABLE_DOES_NOT_EXISTS,
-        HttpStatus.BAD_REQUEST
-      )
+        HttpStatus.BAD_REQUEST,
+      );
     }
     if (updateTableInput.status === ETableStatus.INUSE) {
       return await this.tableRepository.update(
         { id: updateTableInput.tableId },
-        { status: updateTableInput.status, amountOfPeople: updateTableInput.amountOfPeople, openAt: new Date().toISOString() },
+        {
+          status: updateTableInput.status,
+          amountOfPeople: updateTableInput.amountOfPeople,
+          openAt: new Date().toISOString(),
+        },
       );
     } else {
       return await this.tableRepository.update(
         { id: updateTableInput.tableId },
-        { status: updateTableInput.status, amountOfPeople: updateTableInput.amountOfPeople, openAt: null },
-      )
+        {
+          status: updateTableInput.status,
+          amountOfPeople: updateTableInput.amountOfPeople,
+          openAt: null,
+        },
+      );
     }
   }
 
-  async createPayment(userData: IUserData, createPaymentInput: CreatePaymentInput) {
+  async createPayment(
+    userData: IUserData,
+    createPaymentInput: CreatePaymentInput,
+  ) {
+    return await this.dataSource.transaction(
+      async (entityManager: EntityManager) => {
+        const setTableOpen = await entityManager
+          .getRepository(Table)
+          .update(
+            { id: createPaymentInput.tableId },
+            { status: ETableStatus.OPEN, amountOfPeople: 0, openAt: null },
+          );
 
-    return await this.dataSource.transaction( async (entityManager: EntityManager) => {
+        if ((setTableOpen.affected = 0)) {
+          throw new HttpException(
+            ErrorMessage.TABLE_DOES_NOT_EXISTS,
+            HttpStatus.BAD_REQUEST,
+          );
+        }
 
-      const setTableOpen = await entityManager.getRepository(Table).update(
-        { id: createPaymentInput.tableId },
-        { status: ETableStatus.OPEN, amountOfPeople: 0, openAt: null }
-      )
-
-      if (setTableOpen.affected = 0) {
-        throw new HttpException(
-          ErrorMessage.TABLE_DOES_NOT_EXISTS,
-          HttpStatus.BAD_REQUEST
-        )
-      }
-
-      return await this.orderService.updateStatusMany(createPaymentInput.tableId, entityManager)
-    }
-    )
+        return await this.orderService.updateStatusMany(
+          createPaymentInput.tableId,
+          entityManager,
+        );
+      },
+    );
   }
 
+  async updateGroupTableByTableIds(
+    groupId: number,
+    tableIds: number[],
+    entityManager?: EntityManager,
+  ) {
+    const tableRepository = entityManager
+      ? entityManager.getRepository(Table)
+      : this.tableRepository;
+    return await tableRepository.update(
+      { id: In(tableIds) },
+      { status: ETableStatus.GROUPED, groupId },
+    );
+  }
 }
